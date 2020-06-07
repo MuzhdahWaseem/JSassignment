359a403a9efca0fdde08a2b73e76946743532ede
MuzhdahWaseem <64963555+MuzhdahWaseem@users.noreply.github.com>
2020-06-07 17:32:32 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+// Chapter NO 1
+
+// // Q:1 Greeting text:
+// alert("Welcome to JavaScript!")
+
+// // Q:2 Message:
+// alert("Error! Please enter a valid password")
+
+// // Q:3 Message:
+// alert("Welcome to JS Land...\n Happy Coding!")
+
+// // Q:4 Message: (Part-1)
+// alert("Welcome to JS Land")
+
+// // Q:4 Message: (Part-2)
+// alert()
+
+// Q:5 Console:
+// var c = "Hello,,, I can run Js through my web browser's console";
+// console.log(c)
+
+// Chapter 2
+// Q:1
+
+var a = "username";
+document.write(a); "<br>"
+
+// Q:2
+
+var v = "Muzhdah Waseem"
+document.write(v)
+
+
+
+
+
+// Arrys And Loops
+// Q:10
+
+// var x;
+// for(x = 5; x <= 50 ; x+5){
+//     document.write(x,)
+// }
